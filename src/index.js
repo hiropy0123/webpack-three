68b4a88716036f9f41b27a79284abb2c284d2226
HirokiNakashima <hiroki123naka4ma@gmail.com>
2019-10-31 16:22:57 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three'
+
+window.addEventListener('DOMContentLoaded', init)
+
+function init() {
+  // サイズの指定
+  const width = 960
+  const height = 540
+
+  // レンダラーの作成
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#mycanvas')
+  })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(width, height)
+
+  // シーンの作成
+  const scene = new THREE.Scene()
+
+  // カメラの作成
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 2000)
+  camera.position.set(0, 0, +1000)
+
+  // オブジェクトの作成
+  const geometry = new THREE.BoxGeometry(300, 300, 300)
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xFF0000
+  })
+  const box = new THREE.Mesh(geometry, material)
+
+  // シーンに追加
+  scene.add(box)
+
+  // ライト
+  const light = new THREE.DirectionalLight(0xFFFFFF, 3)
+  light.position.set(1, 1, 1)
+  scene.add(light)
+
+  tick()
+  
+  function tick() {
+    box.rotation.x += 0.01
+    box.rotation.y += 0.01
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(tick)
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const WriteFilePlugin   = require('write-file-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// 'production' か 'development' を指定
+const MODE = 'development';
+ 
+const enabledSourceMap = (MODE === 'development');
+
+module.exports = {
+  mode: MODE,
+
+  entry: './src/index.js',
+  output: {
+    filename: `bundle.js`,
+    path: `${__dirname}/dist`
+  },
+ 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'css-loader',
+          'style-loader'
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+ 
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+            }
+          },
+          'import-glob-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // Babel のオプションを指定する
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ],
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.json']
+  },
+  plugins: [
+    new WriteFilePlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: `${__dirname}/static/*`,
+        to: `${__dirname}/dist/`,
+        flatten: true
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`
+    })
+  ],
+  performance: { 
+    hints: false,
+    maxAssetSize: 30000,
+    maxEntrypointSize: 1000 
+  },
+  // externals: {
+  //   // jQueryのレガシープラグインを使用する場合は、
+  //   // jQueryをWebpack内部に取り込まず、外部参照する。
+  //   // HTMLファイルでjqueryとプラグインのファイルを個別読み込み
+  //   'jquery': '$'
+  // },
+  // Configuration for dev server
+  devServer: {
+    contentBase: `${__dirname}/dist/`,
+    watchContentBase: true,
+    open: true,
+    port: 3333
+  },
+};
